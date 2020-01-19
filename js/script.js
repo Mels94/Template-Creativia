32690feed492c361b3a1dcd4877ecf661f07fb71
Mels94 <mels.saribekyan@mail.ru>
2020-01-20 02:09:20 +0400

    change

--- js/script.js
+++ js/script.js
@@ -2,21 +2,36 @@ $(document).ready(function(){
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
 
-        if(value == "all")
+        if(value === "all")
         {
-            $('.filter').show('1000');
+            $('.filter').show(2000);
         }
         else
         {
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
+            $(".filter").not('.'+value).hide(2000);
+            $('.filter').filter('.'+value).show(2000);
         }
+
+        if (!$(this).hasClass('active')) {
+            $(".filter-button").removeClass('active');
+            $(this).addClass('active');
+        }
+
+/*        if ($(this).attr('data-filter') === 'filter-illustration' || 'filter-digital' || 'filter-web') {
+
+            //console.log($('.gallery_product').filter('.filter-illustration'));
+            $('.gallery_product').filter('.filter-illustration').css('height', '204px');
+            $('.gallery_product').filter('.filter-digital').css('height', '204px');
+            $('.gallery_product').filter('.filter-web').css('height', '204px');
+            //$('#portfolio .box2').attr('class','filter-illustration').css('height', '100%');
+
+        }
+        if ($(this).attr('data-filter') === 'all') {
+            $('.gallery_product').css('height', '50%');
+        }*/
+
     });
 
-    if ($(".filter-button").removeClass("active")) {
-        $(this).removeClass("active");
-    }
-    $(this).addClass("active");
-    // $(this).css({color:'#FF0036'});
-    // console.log($(this));
+
+
 });
